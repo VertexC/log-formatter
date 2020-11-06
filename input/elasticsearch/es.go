85d0d3ec56a811911eaacbefb49d03f45b70a779
VertexC <bob2420083992@gmail.com>
2020-11-06 17:01:17 +0800

    add feature: support timely retry for es query

--- input/elasticsearch/es.go
+++ input/elasticsearch/es.go
@@ -5,6 +5,7 @@ import (
 	"context"
 	"encoding/json"
 	"strings"
+	"time"
 
 	"github.com/VertexC/log-formatter/util"
 
@@ -14,9 +15,9 @@ import (
 var logger = new(util.Logger)
 
 type Query struct {
-	Index     string `yaml:"index"`
-	Body      string `yaml:"body"`
-	Formatter string `yaml:"formatter"`
+	Index string `yaml:"index"`
+	Body  string `yaml:"body"`
+	Retry int    `yaml:"retry" default:"0"` // retry qury in every <Retry>s
 }
 
 type EsConfig struct {
@@ -63,59 +64,69 @@ func Execute(input EsConfig, inputCh chan interface{}, logFile string, verbose b
 
 	// Build the request body.
 	for _, query := range input.Quries {
-		var buf bytes.Buffer
+		go func() {
+			for {
+				var buf bytes.Buffer
 
-		if json.Valid([]byte(query.Body)) {
-			buf.WriteString(query.Body)
-		} else {
-			logger.Error.Fatalf("Error encoding query %s to json\n", query.Body)
-		}
+				if json.Valid([]byte(query.Body)) {
+					buf.WriteString(query.Body)
+				} else {
+					logger.Error.Fatalf("Error encoding query %s to json\n", query.Body)
+				}
 
-		// Perform the search request.
-		res, err = es.Search(
-			es.Search.WithContext(context.Background()),
-			es.Search.WithIndex(query.Index),
-			es.Search.WithBody(&buf),
-			es.Search.WithTrackTotalHits(true),
-			es.Search.WithPretty(),
-		)
-		if err != nil {
-			logger.Error.Fatalf("Error getting response: %s", err)
-		}
-		defer res.Body.Close()
-
-		if res.IsError() {
-			var e map[string]interface{}
-			if err := json.NewDecoder(res.Body).Decode(&e); err != nil {
-				logger.Error.Println(res.Body)
-				logger.Error.Fatalf("Error parsing the response body: %s", err)
-			} else {
-				// Print the response status and error information.
-				logger.Error.Fatalf("[%s] %s: %s",
-					res.Status(),
-					e["error"].(map[string]interface{})["type"],
-					e["error"].(map[string]interface{})["reason"],
+				// Perform the search request.
+				res, err = es.Search(
+					es.Search.WithContext(context.Background()),
+					es.Search.WithIndex(query.Index),
+					es.Search.WithBody(&buf),
+					es.Search.WithTrackTotalHits(true),
+					es.Search.WithPretty(),
 				)
+				if err != nil {
+					logger.Error.Fatalf("Error getting response: %s", err)
+				}
+				defer res.Body.Close()
+
+				if res.IsError() {
+					var e map[string]interface{}
+					if err := json.NewDecoder(res.Body).Decode(&e); err != nil {
+						logger.Error.Println(res.Body)
+						logger.Error.Fatalf("Error parsing the response body: %s", err)
+					} else {
+						// Print the response status and error information.
+						logger.Error.Fatalf("[%s] %s: %s",
+							res.Status(),
+							e["error"].(map[string]interface{})["type"],
+							e["error"].(map[string]interface{})["reason"],
+						)
+					}
+				}
+
+				if err := json.NewDecoder(res.Body).Decode(&r); err != nil {
+					logger.Error.Fatalf("Error parsing the response body: %s", err)
+				}
+				// Print the response status, number of results, and request duration.
+				logger.Trace.Printf(
+					"[%s] %d hits; took: %dms",
+					res.Status(),
+					int(r["hits"].(map[string]interface{})["total"].(map[string]interface{})["value"].(float64)),
+					int(r["took"].(float64)),
+				)
+				// Print the ID and document source for each hit.
+				for i, hit := range r["hits"].(map[string]interface{})["hits"].([]interface{}) {
+					logger.Trace.Printf("Return Id %d * ID=%s, %s", i, hit.(map[string]interface{})["_id"], hit.(map[string]interface{})["_source"])
+					msg := hit.(map[string]interface{})["_source"]
+					inputCh <- msg
+				}
+
+				logger.Trace.Println(strings.Repeat("=", 37))
+
+				if query.Retry <= 0 {
+					break
+				}
+				time.Sleep(time.Duration(query.Retry) * time.Second)
+				logger.Info.Println("Retry after sleep")
 			}
-		}
-
-		if err := json.NewDecoder(res.Body).Decode(&r); err != nil {
-			logger.Error.Fatalf("Error parsing the response body: %s", err)
-		}
-		// Print the response status, number of results, and request duration.
-		logger.Trace.Printf(
-			"[%s] %d hits; took: %dms",
-			res.Status(),
-			int(r["hits"].(map[string]interface{})["total"].(map[string]interface{})["value"].(float64)),
-			int(r["took"].(float64)),
-		)
-		// Print the ID and document source for each hit.
-		for i, hit := range r["hits"].(map[string]interface{})["hits"].([]interface{}) {
-			logger.Trace.Printf("Return Id %d * ID=%s, %s", i, hit.(map[string]interface{})["_id"], hit.(map[string]interface{})["_source"])
-			msg := hit.(map[string]interface{})["_source"]
-			inputCh <- msg
-		}
-
-		logger.Trace.Println(strings.Repeat("=", 37))
+		}()
 	}
 }
