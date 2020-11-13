7b70e9ad98886c3f4a73d08a6a480d5c5cce533b
VertexC <bob2420083992@gmail.com>
2020-11-13 15:23:11 +0800

    add feature: add output to file

--- Makefile
+++ Makefile
@@ -22,13 +22,23 @@ services-down:
 kafka-consumer-test: build
 	timeout --preserve-status 20s ./main -c test/kafka-console-test.yml
 
-pipeline-test:
-	$(MAKE) kafka-consumer-test
+.PHONY: file-file-test
+file-file-test: build
+	timeout --preserve-status 20s ./main -c test/file-file-test.yml
+	@[ $(shell wc -l < output-test.txt) -eq $(shell wc -l < test/input-test.txt) ]
+	rm output-test.txt
 
+.PHONY: pipeline-test
+pipeline-test:
+	$(MAKE) services-start
+	sleep 10s
+	$(MAKE) kafka-consumer-test
+	$(MAKE) file-file-test
+	$(MAKE) services-down
+
+.PHONY: local-test
 local-test:
 	@echo "======= start go unit test ======"
 	$(MAKE) go-test
 	@echo "======= start pipeline test ======"
-	$(MAKE) services-start
 	$(MAKE) pipeline-test
-	$(MAKE) services-down
--- output/output.go
+++ output/output.go
@@ -1,14 +1,34 @@
 package output
 
 import (
+	"encoding/json"
 	"github.com/VertexC/log-formatter/output/console"
 	"github.com/VertexC/log-formatter/output/elasticsearch"
+	"log"
+	"os"
 )
 
 type Config struct {
 	Target   string                 `yaml:"target"`
 	EsCfg    elasticsearch.EsConfig `yaml:"elasticsearch,omitempty"`
 	KafkaCfg KafkaConfig            `yaml:"kafka,omitempty"`
+	File     string                 `yaml:"file"`
+}
+
+func writeFile(file string, outputCh chan interface{}) {
+	f, err := os.OpenFile(file, os.O_CREATE|os.O_WRONLY, 0667)
+	if err != nil {
+		log.Fatalf("Failed to open file %s with error:%s \n", file, err)
+	}
+	defer f.Close()
+
+	for doc := range outputCh {
+		data, err := json.Marshal(doc)
+		if err != nil {
+			log.Fatalf("Failed to marshal doc: %+v into json. %s", doc, err)
+		}
+		f.WriteString(string(data) + "\n")
+	}
 }
 
 func Execute(config Config, outputCh chan interface{}, logFile string, verbose bool) {
@@ -17,5 +37,9 @@ func Execute(config Config, outputCh chan interface{}, logFile string, verbose b
 		elasticsearch.Execute(config.EsCfg, outputCh, logFile, verbose)
 	case "console":
 		console.Execute(outputCh)
+	case "file":
+		writeFile(config.File, outputCh)
+	default:
+		panic("Invalid output target:" + config.Target)
 	}
 }
